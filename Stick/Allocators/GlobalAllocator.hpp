3f432645a5327d84bc5aee0f9c108b6516ab15ca
Matthias Dörfelt <hi@mokafolio.de>
2017-06-05 16:15:11 -0700

    added GlobalAllocator for singleton style allocators to seemlessly fit into the template base allocator system

---
+++ Stick/Allocators/GlobalAllocator.hpp
@@ -0,0 +1,37 @@
+#ifndef STICK_ALLOCATORS_GLOBALALLOCATOR_HPP
+#define STICK_ALLOCATORS_GLOBALALLOCATOR_HPP
+
+namespace stick
+{
+    namespace mem
+    {
+        template<class Alloc>
+        class STICK_API GlobalAllocator
+        {
+        public:
+
+            inline Block allocate(Size _byteCount, Size _alignment)
+            {
+                return instance().allocate(_byteCount, _alignment);
+            }
+
+            inline bool owns(const Block & _blk)
+            {
+                return instance().owns(_blk);
+            }
+
+            inline void deallocate(const Block & _blk)
+            {
+                instance().deallocate(_blk);
+            }
+
+            inline Alloc & instance()
+            {
+                static Alloc s_instance;
+                return s_instance;
+            }
+        };
+    }
+}
+
+#endif //STICK_ALLOCATORS_GLOBALALLOCATOR_HPP
